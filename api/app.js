8e24eed39e6ccd7f1256482f92ab4fe1da6edc8c
SabitTheBot <iamsabit99@gmail.com>
2021-07-09 23:12:08 +0600

    file setup done

---
+++ api/app.js
@@ -0,0 +1,45 @@
+//external package
+const express =  require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const  cors = require('cors');
+
+
+require('dotenv').config();
+
+//app
+const app = express();
+
+// import all router
+
+
+
+//middleware
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+//connect to db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then( () =>{ 
+    console.log("Connected To Database");
+} )
+
+
+//routing file
+
+
+
+
+
+//run the server
+const port = process.env.PORT || 8000 ;
+app.listen(port, ()=>{
+    console.log("Server is Running on Port: "+port);
+})
