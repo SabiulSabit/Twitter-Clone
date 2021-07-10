063b7599708f43cb38ba0341d1f79300d794d438
SabitTheBot <iamsabit99@gmail.com>
2021-07-10 13:10:43 +0600

    new tweet create done

--- api/controller/tweet.js
+++ api/controller/tweet.js
@@ -1,6 +1,34 @@
-
+const Tweet = require("../models/Tweet");
 
 //create new tweet
-exports.postCreatNewTweet = (req,res,next) =>{
-    console.log("postCreatNewTweet")
-}
+exports.postCreatNewTweet = (req, res, next) => {
+
+  const { text } = req.body;
+  const authorId = req.profile._id;
+
+  //valid title and content
+  if (text.length <= 0) {
+    return res.status(400).json({
+      error: "Tweet Required",
+    });
+  }
+
+  //create post
+  const post = new Tweet({
+    author: authorId,
+    text: text,
+  });
+
+  post.save((err, data) => {
+    if (err) {
+      //console.log(err)
+      return res.status(400).json({
+        error: err,
+      });
+    }
+
+    return res.json({
+      data,
+    });
+  });
+};
