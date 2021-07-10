809f2dbbda5462d4953d519238dede5b4c3ca41c
SabitTheBot <iamsabit99@gmail.com>
2021-07-10 17:22:52 +0600

    step 2 done

--- ui/src/api/tweet.js
+++ ui/src/api/tweet.js
@@ -2,7 +2,7 @@ import { API } from "./config";
 
 // create new tweet
 export const createTweet = (text,userID, token) => {
-     console.log(text,userID, token)
+    // console.log(text,userID, token)
   
     return fetch(`${API}/tweet/post/${userID}`, {
       method: "POST",
@@ -20,3 +20,25 @@ export const createTweet = (text,userID, token) => {
         return err;
       });
   };
+
+
+  //delete tweet
+  export const deleteTweet = (postID,userID, token) => {
+  //  console.log(postID,userID, token)
+ 
+   return fetch(`${API}/tweet/delete/${postID}/${userID}`, {
+     method: "DELETE",
+     headers: {
+       Accept: "application/json",
+       Authorization: `Bearer ${token}`,
+     },
+  
+   })
+     .then((res) => {
+       return res.json();
+     })
+     .catch((err) => {
+       return err;
+     });
+ };
+  
